0c4e00decdc4d1a0afd1ea46cfb4740aa4db6a74
lorenzo <lorengo@braufficio.com>
2022-07-24 15:08:01 +0200

    Working on controller

--- ros_foxy_ws/src/two_wheel_control/include/two_wheel_control/two_wheel_ID_controller.hpp
+++ ros_foxy_ws/src/two_wheel_control/include/two_wheel_control/two_wheel_ID_controller.hpp
@@ -6,6 +6,9 @@
 #include <controller_interface/controller_interface.hpp>
 #include <two_wheel_control_msgs/msg/command.hpp>
 
+// Linear Algebra utils
+#include<armadillo>
+
 
 namespace two_wheel_id_controller{
     
@@ -14,21 +17,37 @@ namespace two_wheel_id_controller{
             TwoWheelIDController();
             ~TwoWheelIDController();
         
-            controller_interface::return_type init(const std::string &controller_name);
+            controller_interface::return_type init(const std::string &controller_name) override;
             
-            controller_interface::InterfaceConfiguration command_interface_configuration();
-            controller_interface::InterfaceConfiguration state_interface_configuration();
+            controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+            controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
-            controller_interface::return_type on_configure();
-            controller_interface::return_type on_activate();
-            controller_interface::return_type on_deactivate();
+            rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+            rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+            rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
 
-            controller_interface::return_type update();
+            controller_interface::return_type update() override;
 
         private:
-            void subCallback(const two_wheel_control_msgs::msg::Command &msg);
+            void subCallback(const two_wheel_control_msgs::msg::Command::SharedPtr msg);
 
             rclcpp::Subscription<two_wheel_control_msgs::msg::Command>::SharedPtr _sub;
+            arma::Col<double> u(); // measured state col vector [p, thetay, thetaz]
+            arma::Col<double> u_dot();
+            arma::Col<double> u_m(); //real measured col vector [wr, wl, thetay]
+            arma::Col<double> u_m_dot();
+            arma::Col<double> q_w(); // wanted state col vector [p, thetay, thetaz]
+            amra::Col<double> q_w_dot();
+            arma::Col<double> e();  // error
+            arma::Col<double> e_dot();
+            arma::Mat<double> Kp();
+            arma::Mat<double> Kd();
+            arma::Mat<double> B();
+            arma::Col<double> G();
+            arma::Mat<double> T(); // Conversion matrix between gamma input [f, My, Mz] and tau input [Mr, Ml]
+            arma::Mat<double> T_out();  // Conversion matrix between generalized coord [v,thetay,thetaz] and measured output [wr, wl, thetay]
+                                        //[T;[0 0 1]]
+            arma::Col<double> tau(); // input vector of the system  
     };
 
 }
--- ros_foxy_ws/src/two_wheel_control/src/two_wheel_ID_controller.cpp
+++ ros_foxy_ws/src/two_wheel_control/src/two_wheel_ID_controller.cpp
@@ -3,6 +3,7 @@
 
 
 
+
 using namespace two_wheel_id_controller;
 
 TwoWheelIDController::TwoWheelIDController(){
@@ -14,8 +15,25 @@ TwoWheelIDController::~TwoWheelIDController(){
 }
 
 controller_interface::return_type TwoWheelIDController::init(const std::string &controller_name){
-    _sub = this->node_->create_subscription<two_wheel_control_msgs::msg::Command>("two_wheel_command", 10, std::bind(this, &TwoWheelIDController::subCallback, std::placeholders::_1));
-    // TODO: declare params
+    controller_interface::return_type res = controller_interface::ControllerInterface::init(controller_name); 
+    if(res == controller_interface::return_type::ERROR) return res;
+
+    _sub = this->node_->create_subscription<two_wheel_control_msgs::msg::Command>("two_wheel_command", 10, std::bind(&TwoWheelIDController::subCallback, this,  std::placeholders::_1));
+    // TODO: declare params: control strategy(p,v,pv),
+
+    // Allocate memory for matices/vectors
+
+    u.zeros(3);
+    u_dot.zerso(3);
+    q_w.zeros(3);
+    q_w_dot.zeros(3);
+    B.zeros(3,3);
+    G.zeros(3);
+    T.zeros(2,3);
+    T_out.zeros(3,3);
+    Kp.zeros(3,3);
+    Kd.zeros(3,3);
+    tau.zers(2);
 
     return controller_interface::return_type::OK;
 }
@@ -23,7 +41,7 @@ controller_interface::return_type TwoWheelIDController::init(const std::string &
 /*! 
 *   Inverse Dynamic controller only need effort interfaces
 */
-controller_interface::InterfaceConfiguration TwoWheelIDController::command_interface_configuration(){
+controller_interface::InterfaceConfiguration TwoWheelIDController::command_interface_configuration() const{
     controller_interface::InterfaceConfiguration config;
     config.names.push_back("RW_joint/effort");
     config.names.push_back("LW_joint/effort");
@@ -32,31 +50,79 @@ controller_interface::InterfaceConfiguration TwoWheelIDController::command_inter
 }
 
 /*!
-*  
+*  3 feedback configurations:
+*   position
+*   velocity
+*   position,velocity
 */
-controller_interface::InterfaceConfiguration TwoWheelIDController::state_interface_configuration(){
+controller_interface::InterfaceConfiguration TwoWheelIDController::state_interface_configuration() const{
     controller_interface::InterfaceConfiguration config;
+    // TODO: add control strategy
+    if(true){
+        //PV control
+        config.names.push_back("RW_joint/position");
+        config.names.push_back("LW_joint/position");
+        config.names.push_back("imu_sensor/orientation.y");
+        config.names.push_back("RW_joint/velocity");
+        config.names.push_back("LW_joint/velocity");
+        config.names.push_back("imu_sensor/angular_velocity.y");
+        
+        
 
+    }else{
 
+    }
+    config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+
+    return config;
 }
 
-controller_interface::return_type TwoWheelIDController::on_configure(){
-
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn TwoWheelIDController::on_configure(const rclcpp_lifecycle::State & previous_state){
+    // TODO: init B,T*,K* with passed parameters.
+    return CallbackReturn::SUCCESS;
 }
 
-controller_interface::return_type TwoWheelIDController::on_activate(){
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn TwoWheelIDController::on_activate(const rclcpp_lifecycle::State & previous_state){
 
+    return CallbackReturn::SUCCESS;
 }
 
-controller_interface::return_type TwoWheelIDController::on_deactivate(){
+rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn TwoWheelIDController::on_deactivate(const rclcpp_lifecycle::State & previous_state){
 
+    return CallbackReturn::SUCCESS;
 }
 
 controller_interface::return_type TwoWheelIDController::update(){
+    
+    // TODO: insert strategy check
+    if(true){
+        // PV control
+        // Measure current state
+        for(int i = 0; i < 3; i++){
+            u_m(i) = this->state_interfaces_[i].get_value();
+            u_m_dot(i) = this->state_interfaces_[i+3].get_value();
+        }
 
+        u = pinv(T_out) * u_m;
+        u_dot = pinv(T_out) * u_m_dot;
+
+        e = q_w - u;
+        e_dot = q_w_dot - u_dot;
+
+        // TODO: Compute G
+
+        tau = T * ((Kp * e + Kd * e_dot) * B + G); // TODO: check what we need between T, T' and T_inv
+
+        for(int i = 0; i < 2; i++){
+            this->command_interfaces_[i].set_value(tau(i));
+        }
+        
+    }else{
+
+    }
 }
 
-void TwoWheelIDController::subCallback(const two_wheel_control_msgs::msg::Command &msg){
+void TwoWheelIDController::subCallback(const two_wheel_control_msgs::msg::Command::SharedPtr msg){
 
 }
 
